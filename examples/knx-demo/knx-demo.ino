4cea8b2d9dfbec4e0cab39bcfb2a48db117090ee
Marco Scholl <develop@marco-scholl.de>
2025-06-13 00:06:55 +0200

    test

--- examples/knx-demo/knx-demo.ino
+++ examples/knx-demo/knx-demo.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <knx.h>
+#include "TPUart/Interface/ArduinoSerial.h"
 
 #if MASK_VERSION != 0x07B0 && (defined ARDUINO_ARCH_ESP8266 || defined ARDUINO_ARCH_ESP32)
 #include <WiFiManager.h>
