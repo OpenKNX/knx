bd114b6a30a02696a267bd52f06c9ac8560dbbd5
Waldemar Porscha <wp@porscha.eu>
2025-11-26 18:59:53 +0100

    remove alternative implementation for Dpt::sizeInMemory()

--- src/knx/dpt.cpp
+++ src/knx/dpt.cpp
@@ -22,30 +22,6 @@ bool Dpt::operator!=(const Dpt& other) const
     return !(other == *this);
 }
 
-// uint8_t Dpt::sizeInMemory()
-// {
-//     if (mainGroup <= 6 || mainGroup == 17 || mainGroup == 18 || mainGroup == 20 || mainGroup == 21 || mainGroup == 23 || mainGroup == 25 || mainGroup == 26 || mainGroup == 236 || mainGroup == 238) 
-//         return 1;
-//     else if ((mainGroup >= 7 && mainGroup <= 9) || mainGroup == 22 || mainGroup == 207 || mainGroup == 217 || mainGroup == 234 || mainGroup == 237 || mainGroup == 244 || mainGroup == 246)
-//         return 2;
-//     else if (mainGroup == 10 || mainGroup == 11 || mainGroup == 30 || mainGroup == 206 || mainGroup == 225 || mainGroup == 232 || mainGroup == 240 || mainGroup == 250 || mainGroup == 254)
-//         return 3;
-//     else if ((mainGroup >= 12 && mainGroup <= 15) || mainGroup == 27 || mainGroup == 241 || mainGroup == 251)
-//         return 4;
-//     else if (mainGroup == 252)
-//         return 5;
-//     else if (mainGroup == 219 || mainGroup == 222 || mainGroup == 229 || mainGroup == 235 || mainGroup == 242 || mainGroup == 245 || mainGroup == 249)
-//         return 6;
-//     else if (mainGroup == 19 || mainGroup == 29 || mainGroup == 230 || mainGroup == 255 || mainGroup == 275)
-//         return 8;
-//     else if (mainGroup == 16)
-//         return 15; // terminating with 0x00 char
-//     else if (mainGroup == 285)
-//         return 16; 
-//     else
-//         return 1; // default to 1 byte as currently implemented in GroupObject
-// }
-
 unsigned char Dpt::sizeInMemory() const
 {
     switch (mainGroup)
