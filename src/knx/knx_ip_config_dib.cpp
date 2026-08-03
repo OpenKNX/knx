2fa16708033e1d872a15cf90d6bc0b9aedb06843
Erkan Çolak <erkanc@gmx.de>
2026-08-03 21:41:13 +0200

    fix - ip: correct IP-config DIB info1/info2 offsets for the 0x04 current layout
    - IP_CUR_CONFIG (0x04, 20 B): method/reserved at 18/19, DHCP server at 14..17
    
    - branches were swapped -> 0x04 method was written into the DHCP-server field

--- src/knx/knx_ip_config_dib.cpp
+++ src/knx/knx_ip_config_dib.cpp
@@ -56,36 +56,42 @@ void KnxIpConfigDIB::dhcp(uint32_t addr)
     pushInt(addr, _data + 14);
 }
 
+// info1/info2 are the two trailing fields; their offsets differ between the two DIB layouts (03_08_02 Core
+// Figures 16/17):
+//   IP_CONFIG      (0x03, 16 B): ...gateway[10..13], IP Capabilities[14], IP assignment method[15]
+//   IP_CUR_CONFIG  (0x04, 20 B): ...gateway[10..13], DHCP Server[14..17], Current IP assignment method[18], Reserved[19]
+// Hence current -> 18/19, non-current -> 14/15. (These two branches were previously swapped, which wrote the
+// 0x04 method into the DHCP-server field and read/wrote the 0x03 fields out of the 16-byte DIB.)
 uint8_t KnxIpConfigDIB::info1()
 {
     if(_isCurrent)
-        return _data[14];
-    else
         return _data[18];
+    else
+        return _data[14];
 }
 
 void KnxIpConfigDIB::info1(uint8_t addr)
 {
     if(_isCurrent)
-        _data[14] = addr;
-    else
         _data[18] = addr;
+    else
+        _data[14] = addr;
 }
 
 uint8_t KnxIpConfigDIB::info2()
 {
     if(_isCurrent)
-        return _data[15];
-    else
         return _data[19];
+    else
+        return _data[15];
 }
 
 void KnxIpConfigDIB::info2(uint8_t addr)
 {
     if(_isCurrent)
-        _data[15] = addr;
-    else
         _data[19] = addr;
+    else
+        _data[15] = addr;
 }
 
 #endif
