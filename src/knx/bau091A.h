904abcc1778b63d4fc1deb140aabda9502808afb
Erkan Çolak <erkanc@gmx.de>
2026-08-16 19:22:06 +0200

    knx: expose IpTunnelServer accessor on Bau091A
    - getIpTunnelServer() under KNX_TUNNELING

--- src/knx/bau091A.h
+++ src/knx/bau091A.h
@@ -22,6 +22,10 @@ class Bau091A : public BauSystemBCoupler, public ITpUartCallBacks, public DataLi
 
     IpDataLinkLayer* getPrimaryDataLinkLayer();
     TpUartDataLinkLayer* getSecondaryDataLinkLayer();
+
+#ifdef KNX_TUNNELING
+    IpTunnelServer& getIpTunnelServer() { return _ipTunnelServer; }
+#endif
 #ifdef OPENKNX_FTC
     // FTC fast-transfer flow control: read the TP transmit FIFO depth off the secondary (TP) link.
     uint16_t ftcTxQueueSize() override;
